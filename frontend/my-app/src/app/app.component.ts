8f83b0337ab41eff11e8e73889f460869b3d508e
calebkumar <kumarcal@grinnell.edu>
2018-08-22 15:27:46 -0700

    Front-end connected to all endpoints

--- frontend/my-app/src/app/app.component.ts
+++ frontend/my-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {RequestOptions} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,37 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Iris Prediction Modeling';
 
   constructor(private http: HttpClient) {
-    console.log('Constructing app component');
-    this.getData();
+    // this.trainModel();
   }
 
-  public getData() {
+  public trainModel() {
     return this.http.get('/train').subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  public makePrediction() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    // console.log(headers);
+    const payload = JSON.stringify({'sepal_length': 5, 'sepal_width': 3, 'petal_length': 1, 'petal_width': 1});
+
+    return this.http.post('/predict', payload, {'headers': headers}).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  public getPredictions() {
+    return this.http.get('/predictions').subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+  public deletePredictions() {
+    return this.http.delete('/predictions').subscribe((data: any) => {
       console.log(data);
     });
   }
